8ebd3f395e95e2d57aa8f853a792b14ac2a4bb1c
Illbeblack <shvartsdenis@mail.ru>
2023-06-05 20:23:02 +0300

    feat: refactoring code

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ let secretNum;
 let score = 20;
 let bestScore = 0;
 
+//Secret number
+
 function getSecretNum () {
     secretNum = Math.trunc(Math.random() * 20) + 1;
 };
 getSecretNum ();
+
+//Change text in guess-message
+
+function displayGuessMessage (text) {
+    document.querySelector('.guess-message').textContent = text;
+}
 
 document.querySelector('.check').addEventListener('click',function () {
     const enteredNum = Number(document.querySelector('.number-input').value);
     // No input
 
     if (!enteredNum) {
-        document.querySelector('.guess-message').textContent = "ENTER NUMBER!";
+        displayGuessMessage ("ENTER NUMBER!");
     }
 
     // Plaer won
 
     else if (enteredNum === secretNum) {
-        document.querySelector('.guess-message').textContent = "You're God damn Right!";
+        displayGuessMessage ("You're God damn Right!");
         document.querySelector('.question').textContent = secretNum;
         document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
         document.querySelector('.question').style.width = '50rem';
@@ -30,29 +38,16 @@ document.querySelector('.check').addEventListener('click',function () {
         }
     }
 
-    //To high
+    // Wrong entered number
 
-    else if (enteredNum > secretNum) {
+    else if(enteredNum !== secretNum) {
         if (score > 1) {
-            document.querySelector('.guess-message').textContent = "Overdo!";
+            displayGuessMessage (enteredNum > secretNum ? "Overdo!" : "It's not enough!");
             score--;
             document.querySelector('.score').textContent = score;
         }
         else {
-            document.querySelector('.guess-message').textContent = "Game over";
-            document.querySelector('.score').textContent = 0;
-        }
-    }
-
-    // Too low
-    else if (enteredNum < secretNum) {
-        if (score > 1) {
-            document.querySelector('.guess-message').textContent = "It's not enough!";
-            score--;
-            document.querySelector('.score').textContent = score;
-        }
-        else {
-            document.querySelector('.guess-message').textContent = "Game over";
+            displayGuessMessage ("Game over");
             document.querySelector('.score').textContent = 0;
         }
     }
@@ -68,7 +63,7 @@ document.querySelector('.again').addEventListener('click', function () {
     //default html
 
     document.querySelector('.question').textContent = '???';
-    document.querySelector('.guess-message').textContent = "Let's start!";
+    displayGuessMessage ("Let's start!");
     document.querySelector('.number-input').value = '';
     document.querySelector('.score').textContent = 20;
     score = 20;
