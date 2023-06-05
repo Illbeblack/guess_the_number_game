593e6087ed3fc7f705c7ba9fa91b052e59633374
Illbeblack <shvartsdenis@mail.ru>
2023-06-05 19:33:18 +0300

    feat: make all features

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 'use strict';
-const secretNum = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
 
-document.querySelector('.question').textContent = secretNum;
+let secretNum;
+let score = 20;
+let bestScore = 0;
+
+function getSecretNum () {
+    secretNum = Math.trunc(Math.random() * 20) + 1;
+};
+getSecretNum ();
 
 document.querySelector('.check').addEventListener('click',function () {
     const enteredNum = Number(document.querySelector('.number-input').value);
-    
     // No input
 
     if (!enteredNum) {
@@ -17,8 +21,13 @@ document.querySelector('.check').addEventListener('click',function () {
 
     else if (enteredNum === secretNum) {
         document.querySelector('.guess-message').textContent = "You're God damn Right!";
+        document.querySelector('.question').textContent = secretNum;
         document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
         document.querySelector('.question').style.width = '50rem';
+        if (score > bestScore) {
+            bestScore = score;
+            document.querySelector('.best-score').textContent = bestScore;
+        }
     }
 
     //To high
@@ -47,4 +56,25 @@ document.querySelector('.check').addEventListener('click',function () {
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+//Restart button
+
+document.querySelector('.again').addEventListener('click', function () {
+    //new secret number
+
+    getSecretNum ();
+
+    //default html
+
+    document.querySelector('.question').textContent = '???';
+    document.querySelector('.guess-message').textContent = "Let's start!";
+    document.querySelector('.number-input').value = '';
+    document.querySelector('.score').textContent = 20;
+    score = 20;
+
+    //default style
+
+    document.querySelector('.question').style.width = '25rem';
+    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
 });
